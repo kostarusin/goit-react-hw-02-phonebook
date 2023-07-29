f0bdd6591f6eba755a01f6f08b8e996352095896
Kostyantyn <kosta.rusin@gmail.com>
2023-07-29 13:56:28 +0200

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,30 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
-import style from './ContactList.module.css'
+import style from './ContactList.module.css';
 
-
-class ContactList extends Component {
-  render() {
-    const { contacts, onDeleteContact } = this.props;
-
-    return (
-      <>
-        <ul className={style.list}>
-          {contacts.map(contact => {
-            const { id, name, number } = contact;
-            return (
-                
-              <li className={style.listEl} key={id}>
-                {name}: {number}
-                <div className={style.listBtnWrap}><button type='button' className={style.listBtn} onClick={() => onDeleteContact(id)}>Delete</button></div></li>
-            );
-          })}
-        </ul>
-      </>
-    );
-  }
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <>
+      <ul className={style.list}>
+        {contacts.map(contact => {
+          const { id, name, number } = contact;
+          return (
+            <li className={style.listEl} key={id}>
+              {name}: {number}
+              <div className={style.listBtnWrap}>
+                <button
+                  type="button"
+                  className={style.listBtn}
+                  onClick={() => onDeleteContact(id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 ContactList.propTypes = {
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,26 +1,21 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import style from './Filter.module.css';
 
-class Filter extends Component {
-  
-  render() {
-    const { value, onFilter } = this.props;
-    return (
-      <>
-        <label className={style.label}>
-          Find contacts by name{' '}
-          <input
-            className={style.input}
-            type="text"
-            value={value}
-            onChange={onFilter}
-          />
-        </label>
-      </>
-    );
-  }
-}
+const Filter = ({ value, onFilter }) => {
+  return (
+    <>
+      <label className={style.label}>
+        Find contacts by name{' '}
+        <input
+          className={style.input}
+          type="text"
+          value={value}
+          onChange={onFilter}
+        />
+      </label>
+    </>
+  );
+};
 
 Filter.propTypes = {
   value: PropTypes.string.isRequired,
